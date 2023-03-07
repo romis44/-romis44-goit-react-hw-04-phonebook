d268e603f3a4eb6dadf73735f227a29985d09234
romis44 <110257777+romis44@users.noreply.github.com>
2023-03-07 18:09:27 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize/modern-normalize.css';
+import App from './App';
+import './index.scss';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
